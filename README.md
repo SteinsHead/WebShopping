f351b5cdf571faf8cd6133b9ffe4481e4dba5eaa
SteinsHead <1310605444@qq.com>
2020-05-09 15:26:56 +0800

    第一次尝试提交项目，可以进行编辑了

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # WebShopping
 This is a repository of my class homework.
+
+***
+## 第一次提交
+先把SpringBoot的Demo上传到GitHub上，可以进行编辑和修改了
+***
---
+++ christina/src/test/java/com/example/christina/ChristinaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.christina;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChristinaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
