7527afd9cd16e13f88729a85f3c82c0e58e109b9
SteinsHead <1310605444@qq.com>
2020-05-17 13:56:40 +0800

    尝试编写接口查询数据库并成功

--- christina/src/main/java/com/example/christina/Repository/GoodsRepository.java
+++ christina/src/main/java/com/example/christina/Repository/GoodsRepository.java
@@ -3,6 +3,8 @@ package com.example.christina.Repository;
 import com.example.christina.enity.Goods;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface GoodsRepository extends JpaRepository<Goods, Integer> {
+import java.util.List;
 
+public interface GoodsRepository extends JpaRepository<Goods, Integer> {
+    List<Goods> findBygoodname(String goodname);
 }
--- christina/src/main/java/com/example/christina/controller/GoodsController.java
+++ christina/src/main/java/com/example/christina/controller/GoodsController.java
@@ -3,7 +3,11 @@ package com.example.christina.controller;
 import com.example.christina.Repository.GoodsRepository;
 import com.example.christina.enity.Goods;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.QueryHints;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(value = "goods")
@@ -13,12 +17,17 @@ public class GoodsController {
     private GoodsRepository goodsRepository;
 
     @PostMapping(path = "addGoods")
-    public void addGoods(Goods goods){
+    public void addGoods(@RequestBody Goods goods){
         goodsRepository.save(goods);
     }
 
     @DeleteMapping(path = "deleteGoods")
-    public void deleteGoods(Goods goods){
+    public void deleteGoods(@RequestBody Goods goods){
         goodsRepository.delete(goods);
+    }
+
+    @GetMapping(path = "getGoods")
+    public List<Goods> getGoods(String goodname){
+        return goodsRepository.findBygoodname(goodname);
     }
 }
