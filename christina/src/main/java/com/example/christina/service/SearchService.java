6c2abd995e8597091150da5f4f3718cf67d5a610
SteinsHead <1310605444@qq.com>
2020-05-30 21:02:55 +0800

    上传service

--- christina/src/main/java/com/example/christina/service/SearchService.java
+++ christina/src/main/java/com/example/christina/service/SearchService.java
@@ -1,5 +1,22 @@
 package com.example.christina.service;
 
+import com.example.christina.controller.GoodsController;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
 public class SearchService {
+
+    @Autowired
+    private GoodsController goodsController;
+
+    /*
+    * 查询商品操作
+     */
+    public List<?> Search(String goodsname) {
+        return goodsController.getGoods(goodsname);
+    }
 
 }
