34e10212ac1a7f8e169e4d320844bb5cb25e2add
SteinsHead <1310605444@qq.com>
2020-05-30 21:13:04 +0800

    随便写了一点

--- christina/src/main/java/com/example/christina/service/SearchService.java
+++ christina/src/main/java/com/example/christina/service/SearchService.java
@@ -1,6 +1,8 @@
 package com.example.christina.service;
 
 import com.example.christina.controller.GoodsController;
+import com.example.christina.controller.StoreController;
+import com.example.christina.enity.Store;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -12,11 +14,21 @@ public class SearchService {
     @Autowired
     private GoodsController goodsController;
 
+    @Autowired
+    private StoreController storeController;
+
     /*
     * 查询商品操作
      */
-    public List<?> Search(String goodsname) {
+    public List<?> SearchGood(String goodsname) {
         return goodsController.getGoods(goodsname);
+    }
+
+    /*
+    * 查询店铺操作
+     */
+    public List<?> SearchStore(String storename) {
+        return storeController.getStores(storename);
     }
 
 }
